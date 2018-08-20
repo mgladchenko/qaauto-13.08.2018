1e6a03deebfa959af7847f98fdee58ffcd681f44
Mykola Gladchenko <mykola.gladchenko@gmail.com>
2018-08-20 14:39:02 +0300

    Lesson 3 commit

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -1,7 +1,11 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static java.lang.Thread.sleep;
 
 public class LinkedinLoginTest {
 
@@ -16,11 +20,62 @@ public class LinkedinLoginTest {
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.linkedin.com/");
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/", "Login page URL is wrong.");
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/",
+                "Login page URL is wrong.");
+        Assert.assertEquals(driver.getTitle(), "LinkedIn: Log In or Sign Up",
+                "Login page Title is wrong.");
 
+        WebElement userEmailField = driver.findElement(By.xpath("//input[@id='login-email']"));
+        WebElement userPasswordField = driver.findElement(By.xpath("//input[@id='login-password']"));
+        WebElement signInButton = driver.findElement(By.xpath("//input[@id='login-submit']"));
 
+        Assert.assertTrue(signInButton.isDisplayed(),
+                "signIn button is not displayed on Login page.");
 
+        userEmailField.sendKeys("linkedin.tst.yanina@gmail.com");
+        userPasswordField.sendKeys("Yanina123");
+        signInButton.click();
 
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/feed/",
+                "Home page URL is wrong.");
+        Assert.assertEquals(driver.getTitle(), "LinkedIn",
+                "Home page Title is wrong.");
+
+        WebElement profileNavItem = driver.findElement(By.xpath("//li[@id='profile-nav-item']"));
+        Assert.assertTrue(profileNavItem.isDisplayed(),
+                "profileNavItem button is not displayed on Home page.");
+    }
+
+    @Test
+    public void negativeLoginTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/");
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/",
+                "Login page URL is wrong.");
+        Assert.assertEquals(driver.getTitle(), "LinkedIn: Log In or Sign Up",
+                "Login page Title is wrong.");
+
+        WebElement userEmailField = driver.findElement(By.xpath("//input[@id='login-email']"));
+        WebElement userPasswordField = driver.findElement(By.xpath("//input[@id='login-password']"));
+        WebElement signInButton = driver.findElement(By.xpath("//input[@id='login-submit']"));
+
+        Assert.assertTrue(signInButton.isDisplayed(),
+                "signIn button is not displayed on Login page.");
+
+        userEmailField.sendKeys("a@b.c");
+        userPasswordField.sendKeys("wrong");
+        signInButton.click();
+
+        try {
+            sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement alertMessage = driver.findElement(By.xpath("//div[@role='alert']"));
+        Assert.assertEquals(alertMessage.getText(),
+                "There were one or more errors in your submission. Please correct the marked fields below.",
+                "Alert message text is wrong.");
 
 
     }
