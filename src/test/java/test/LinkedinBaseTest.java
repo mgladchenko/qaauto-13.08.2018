a4eae67c45bff720115bb68135169d27db4f778b
Mykola Gladchenko <mykola.gladchenko@gmail.com>
2018-09-21 13:59:23 +0300

    Lesson #11

--- src/test/java/test/LinkedinBaseTest.java
+++ src/test/java/test/LinkedinBaseTest.java
@@ -2,7 +2,9 @@ package test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import page.LinkedinLoginPage;
@@ -11,10 +13,26 @@ public class LinkedinBaseTest {
     WebDriver driver;
     LinkedinLoginPage linkedinLoginPage;
 
+    String browserName = "chrome";
+
     @BeforeMethod
-    public void beforeMethod() {
-        WebDriverManager.firefoxdriver().setup();
-        driver = new FirefoxDriver();
+    public void beforeMethod() throws Exception {
+        switch (browserName.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "ie":
+                WebDriverManager.iedriver().setup();
+                driver = new InternetExplorerDriver();
+                break;
+            default:
+                throw new Exception("Browser " + browserName + " is not supported.");
+        }
         driver.get("https://www.linkedin.com/");
         linkedinLoginPage = new LinkedinLoginPage(driver);
     }
